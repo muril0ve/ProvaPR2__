a49d34e860f198bdcebb0c43e326a451413699b4
muril0ve <murilo.coutinho@aluno.ifsp.edu.br>
2023-04-19 12:10:01 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaPR2__/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaPR2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int nmr, valorAbsoluto;
+            //Console.WriteLine("digite o valor do nmr: ");
+            //nmr=int.Parse(Console.ReadLine());
+
+            //if (nmr>=0)
+            //{
+            //    valorAbsoluto = nmr;
+            //}
+            //else
+            //{
+            //    valorAbsoluto= - nmr;    
+            //}
+            //Console.WriteLine("O valor absoluto de {0} é {1} ", + nmr, + valorAbsoluto);
+
+
+            //exerc2
+
+            //int nmr;
+            //Console.WriteLine("digite um numero inteiro: ");
+            //nmr = int.Parse(Console.ReadLine());
+            //if (nmr % 3 == 0 && nmr % 5 == 0)
+            //{
+            //    Console.WriteLine("O número é múltiplo de 3 e 5.");
+            //}
+            //else if (nmr % 4 == 0 && nmr % 7 == 0)
+            //{
+            //    Console.WriteLine("O número é múltiplo de 4 e 7.");
+            //}
+            //else if (nmr % 3 == 0)
+            //{
+            //    Console.WriteLine("O número é divisível por 3.");
+            //}
+            //else if (nmr % 4 == 0)
+            //{
+            //    Console.WriteLine("O número é divisível por 4.");
+            //}
+            //else if (nmr % 5 == 0)
+            //{
+            //    Console.WriteLine("O número é divisível por 5.");
+            //}
+            //else if (nmr % 7 == 0)
+            //{
+            //    Console.WriteLine("O número é divisível por 7.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("O número não é divisível por 3, 4, 5 ou 7.");
+
+
+            //exerc3
+            //       int a, b ,c;
+            //   Console.WriteLine("digite o primeiro nmr: ");
+            //   a = int.Parse(Console.ReadLine());
+            //   Console.WriteLine("digite o segundo numero: ");
+            //   b = int.Parse(Console.ReadLine());
+            //   Console.WriteLine("digite o terceiro numero: ");
+            //   c = int.Parse(Console.ReadLine());
+
+
+
+            //   if (a < b && b < c)
+            //       Console.WriteLine(a + " " + b + " " + c);
+            //   else if (a > b && b > c)
+            //       Console.WriteLine(c + " " + b + " " + a);
+            //   else if (b > a && a > c)
+            //       Console.WriteLine(c + " " + a + " " + b);
+            //   else if (b < a && a < c)
+            //       Console.WriteLine(b + " " + a + " " + c);
+            //   else if (a < c && c < b)
+            //       Console.WriteLine(a + " " + c + " " + b);
+            //   else if (b < c && c < a)
+            //       Console.WriteLine(b + " " + c + " " + a);
+
+
+
+
+            //exerc4
+            //int n1;
+            //Console.WriteLine("digite o primeiro nmr: ");
+            //n1 = int.Parse(Console.ReadLine());
+
+
+
+            //switch (n1)
+            //{
+            //    case 1:
+            //        if (n1 == 1)
+            //        {
+            //            Console.WriteLine("domingo " + " Dia de descanso <3, dia de nao fazer nada e ficar em casa");
+
+            //        }
+            //        break;
+
+            //        case 2:
+            //        if (n1 == 2)
+            //        {
+            //            Console.WriteLine("segunda");
+
+            //        } 
+            //        break;
+
+            //    case 3:
+            //        if (n1 == 3)
+            //        {
+            //            Console.WriteLine("terça");
+
+            //        }
+            //        break;
+
+            //    case 4:
+            //        if (n1 == 4)
+            //        {
+            //            Console.WriteLine("quarta");
+
+            //        }
+            //    break;
+
+            //    case 5:
+            //        if (n1 == 5)
+            //        {
+            //            Console.WriteLine("quinta");
+
+            //        }
+            //        break;
+            //    case 6:
+            //        if (n1 == 6)
+            //        {
+            //            Console.WriteLine("sexta " + " ultima dia da semana, o mais satisfatório");
+
+            //        }
+            //        break;
+            //    case 7:
+            //        if (n1 == 7)
+            //        {
+            //            Console.WriteLine("sabado " + " Dia de sair de bicicleta, fazer quele pedal etc.");
+
+            //        }
+            //        break;
+            //    case 8:
+            //        if (n1 >= 8)
+            //        {
+            //            Console.WriteLine( "o dia da semana nao existe" );
+            //        }
+            //        break;
+            //}
+
+
+
+
+
+            //exerc5
+            //int nmr, nmrMa,nmrMe ;
+
+            //for (int i = 0; i >= 0; i++)
+            //{
+            //    Console.WriteLine("digite um numero: ");
+            //    nmr = int.Parse(Console.ReadLine());
+            //    i = nmr;
+
+
+
+
+
+
+
+            //    if (nmr == 0)
+            //    { break; }
+
+            //}
+
+
+            //exerc6
+            //int nmr, s = 0;
+            //Console.Write("Digite um número inteiro: ");
+            //nmr = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i < nmr; i++)
+            //{
+            //    if (nmr % i == 0)
+            //    {
+            //        s += i;
+            //    }
+            //}
+
+            //if (nmr == s)
+            //{
+            //    Console.WriteLine("{0} é um número perfeito.", nmr);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("{0} não é um número perfeito.", nmr);
+            //}
+            //Console.ReadLine();
+
+            //exerc 7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
